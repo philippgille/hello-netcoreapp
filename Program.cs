8642abe0f193a16df973ea9cc12f42109e39fe58
Philipp Gille <philipp.gille@gmail.com>
2017-04-15 13:10:11 +0200

    Initial commit after executing "dotnet new console"

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_netcoreapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
