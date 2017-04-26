30828467cb4f5ddd25eef8db031742f6d6643d77
Philipp Gille <philipp.gille@gmail.com>
2017-04-26 23:52:17 +0200

    Add scripts for publishing via Docker container
    - Uses the official .NET Core SDK Docker container, so that .NET Core doesn't need to be installed on the host machine
    - Didn't test publish-docker.ps1 yet

---
+++ publish-docker.ps1
@@ -0,0 +1,4 @@
+# Creates release artifacts for FDD (framework-dependent deployment)
+# Uses the official .NET Core Docker container that contains the SDK
+
+docker run --rm -v ${PWD}:/root/app -w /root/app microsoft/dotnet:1.1-sdk ./publish.sh
