52d5b1ea4b776eb26c112903b3dd34b2f7e4dfe6
Philipp Gille <philipp.gille@gmail.com>
2017-04-26 23:50:34 +0200

    Improve publish scripts
    - Tested publish.sh in a Docker container, where "dotnet publish" publishes to a different directory, producing errors in the rest of the script
    - Now publish.sh uses the "-o" option for "dotnet publish" to define its own output dir
    - publish.ps1 should behave the same way, so I changed that as well
    - Replaced CRLF by LF in the publish.sh, because otherwise it couldn't be executed in the Docker container
    - Other simplifications and improvements

--- publish.ps1
+++ publish.ps1
@@ -1,21 +1,33 @@
-# Creates release artifacts for FDD (framework-dependent deployment)
+# Creates release artifacts for FDD (framework-dependent deployment).
 # Uses an installed version of .NET Core.
-# .NET Core SDK version should be 1.1
+# .NET Core SDK version should be 1.1.
+# Script must be called from the directory where the script is located.
+
+$appname = "hello-netcoreapp"
+
+# Don't change anything below this line ########################################
 
 $ErrorActionPreference = "Stop"
 
-$appname = "hello-netcoreapp_netcoreapp1.1"
+$publishName = "${appname}_netcoreapp1.1"
+$publishDir = "$PWD\bin\publish"
+
+# Clean and create directories
+If (Test-Path "$publishDir") {Remove-Item "$publishDir"}
+mkdir "$publishDir"
+mkdir "$publishDir\$publishName"
 
 # Restore NuGet packages
 dotnet restore
 
-# Publishes FDD files to .\bin\MCD\release\netcoreapp1.1\hello-netcoreapp.dll
-dotnet publish -f netcoreapp1.1 -c release
+# "dotnet publish" without "-o" option publishes to different directories on Windows vs. .NET Core SDK Docker container.
+# That doesn't matter in this PowerShell script per say, because the publish.sh bash script gets used for that,
+# but if that script needs to adapt to that, let's keep the behavior similar and publish to the same directories.
+dotnet publish -f netcoreapp1.1 -c release -o "$publishDir\$publishName"
 
 # Create an archive with all FDD files for publishing.
 # Requires the full .NET framework to be installed, so that the required assembly can be loaded, which is not part of PowerShell.
-$source = "$PWD\bin\MCD\release\netcoreapp1.1\publish"
-$destination = "$PWD\bin\$appname.zip"
+$destination = "$publishDir\$publishName.zip"
 If (Test-Path $destination) {Remove-Item $destination}
 Add-Type -Assembly "System.IO.Compression.FileSystem"
-[io.compression.zipfile]::CreateFromDirectory($source, $destination)
+[io.compression.zipfile]::CreateFromDirectory("$publishDir\$publishName", $destination)
--- publish.sh
+++ publish.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-set -euxo pipefail
 
-# Creates release artifacts for FDD (framework-dependent deployment)
+# Creates release artifacts for FDD (framework-dependent deployment).
 # Uses an installed version of .NET Core.
+# .NET Core SDK version should be 1.1.
+# Script must be called from the directory where the script is located.
+#
+# Note: No "-o pipefail" option for the bash script,
+# because when used in the .NET Core SDK Docker container this leads to "invalid option nameet: pipefail".
 
-APPNAME="hello-netcoreapp_netcoreapp1.1"
+APPNAME="hello-netcoreapp"
+
+# Don't change anything below this line ########################################
+
+set -eux
+
+PUBLISHNAME="${APPNAME}_netcoreapp1.1"
+PUBLISHDIR="$PWD/bin/publish"
+
+rm -r -f "$PUBLISHDIR"
+mkdir "$PUBLISHDIR"
+mkdir "$PUBLISHDIR/$PUBLISHNAME"
 
 # Restore NuGet packages
 dotnet restore
 
-# Publishes FDD files to .\bin\MCD\release\netcoreapp1.1\hello-netcoreapp.dll
-dotnet publish -f netcoreapp1.1 -c release
+# "dotnet publish" without "-o" option publishes to different directories on Windows vs. .NET Core SDK Docker container.
+dotnet publish -f netcoreapp1.1 -c release -o "$PUBLISHDIR/$PUBLISHNAME"
 
 # Create an archive with all FDD files for publishing.j
 # tar includes the full path when doing `tar -czf $DESTINATION $SOURCE`
 # and a "." when doing `tar -czf $DESTINATION -C $SOURCE .`.
 # So work around that.
 #TODO: Find and use a way to just archive the files without any path or ".""
-SOURCE="$PWD/bin/MCD/release/netcoreapp1.1"
-mv $SOURCE/publish $SOURCE/$APPNAME
-DESTINATION="$PWD/bin/$APPNAME.tar.gz"
-tar -czf $DESTINATION -C $SOURCE $APPNAME
-mv $SOURCE/$APPNAME $SOURCE/publish
+DESTINATION="$PUBLISHDIR/$PUBLISHNAME.tar.gz"
+tar -czf $DESTINATION -C $PUBLISHDIR $PUBLISHNAME
