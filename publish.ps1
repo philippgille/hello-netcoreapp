99389511721023fe57c150e17bc0f13ca91147ed
Philipp Gille <philipp.gille@gmail.com>
2017-04-26 21:25:00 +0200

    Improve PowerShell publish script
    - Add "exit on error"
    - Make app name variable
    - Add comments

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,10 @@
 # Creates release artifacts for FDD (framework-dependent deployment)
+# Uses an installed version of .NET Core.
+# .NET Core SDK version should be 1.1
+
+$ErrorActionPreference = "Stop"
+
+$appname = "hello-netcoreapp_netcoreapp1.1"
 
 # Restore NuGet packages
 dotnet restore
@@ -7,8 +13,9 @@ dotnet restore
 dotnet publish -f netcoreapp1.1 -c release
 
 # Create an archive with all FDD files for publishing.
+# Requires the full .NET framework to be installed, so that the required assembly can be loaded, which is not part of PowerShell.
 $source = "$PWD\bin\MCD\release\netcoreapp1.1\publish"
-$destination = "$PWD\bin\hello-netcoreapp_netcoreapp1.1.zip"
+$destination = "$PWD\bin\$appname.zip"
 If (Test-Path $destination) {Remove-Item $destination}
 Add-Type -Assembly "System.IO.Compression.FileSystem"
 [io.compression.zipfile]::CreateFromDirectory($source, $destination)
