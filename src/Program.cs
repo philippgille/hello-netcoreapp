e808a93a88b62ef413c06484f262813321753cf5
Philipp Gille <philipp.gille@gmail.com>
2017-10-03 21:58:45 +0200

    Add example code for working with a library from a NuGet package

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+// using Newtonsoft.Json.Linq;
 
 namespace hello_netcoreapp
 {
@@ -7,6 +8,10 @@ namespace hello_netcoreapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Use Newtonsoft.Json as an example usage of a library from a NuGet package
+            // JObject jo = new JObject();
+            // jo.Add("foo", JValue.CreateString("bar"));
+            // Console.WriteLine(jo.ToString());
         }
     }
 }
